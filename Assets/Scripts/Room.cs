a70938aa53a7481f928ac4c30155f246449ed205
Michal <malpa.w.klapkach@gmail.com>
2023-09-30 10:00:54 +0200

    Create levels with probuilder

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[SelectionBase]
+public class Room : MonoBehaviour
+{
+    private const float halfSqrt3 = 0.86602540378f;
+
+    [SerializeField]
+    private float size;
+    [SerializeField]
+    private float wallThickness;
+
+    [Space]
+    [SerializeField]
+    private Transform[] wallsPositions;
+
+    private void OnValidate()
+    {
+        foreach (var wall in wallsPositions)
+        {
+            wall.localPosition = (size / 2 * halfSqrt3 - wallThickness / 2)* Vector3.forward;
+        }
+    }
+}
