b9842ed6a466607f0f59f074c3256db5c52b448e
Michal <malpa.w.klapkach@gmail.com>
2023-10-01 03:25:04 +0200

    Add components to angel

---
+++ Assets/Scripts/CircleMovement.cs
@@ -0,0 +1,27 @@
+﻿using Bipolar.LoopedRooms;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class CircleMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float radius;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private SynchronizedTransform synchronizedTransform;
+
+    [SerializeField, ReadOnly]
+    private float angle;
+
+    private void Update()
+    {
+        angle += Time.deltaTime * speed;
+        if (angle > 180)
+            angle -= 360;
+        else if (angle < -180)
+            angle += 360;
+
+        synchronizedTransform.LocalPosition = Quaternion.AngleAxis(angle, Vector3.up) * Vector3.forward * radius;
+    }
+}
